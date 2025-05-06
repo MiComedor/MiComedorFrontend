cc715329f68d422247b45af9832fc0b704b45d31
Mariela <marielammcv.3@gmail.com>
2025-05-06 16:09:28 -0500

    welcome

--- mi-comedor/src/components/welcome/Welcome.tsx
+++ mi-comedor/src/components/welcome/Welcome.tsx
@@ -3,12 +3,12 @@ import './Welcome.css';
 
 const Welcome: React.FC = () => {
     return (
-        <div className="welcome-container">
-            <h1 className="welcome-title">BIENVENIDOS</h1>
-            <img 
-            src="/assets/logo.png" 
-            alt="Logo" 
-            className="welcome-logo" 
+        <div className="d-flex flex-column justify-content-center align-items-center vh-100 text-center">
+            <h1 className="titulo-Pderecho  display-4">BIENVENIDOS</h1>
+            <img
+                src="/assets/logo.png"
+                alt="Logo"
+                className="welcome-logo mt-3 img-fluid"
             />
         </div>
     );
