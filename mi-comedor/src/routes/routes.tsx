9a97c9c4f32afbf4414975f826a901a0f0c8ad55
Mariela <marielammcv.3@gmail.com>
2025-05-06 02:07:38 -0500

    create welcome

--- mi-comedor/src/routes/routes.tsx
+++ mi-comedor/src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Routes, Route} from "react-router-dom";
 
+//import WelcomePage from "../pages/WelcomePage";
 import Profile from "../components/Profile/Profile";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
@@ -9,6 +10,7 @@ const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
+      {/* <Route path="/welcome" element={<WelcomePage />} /> */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/profile" element={<Profile />} />
