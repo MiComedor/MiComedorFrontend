280737bbd6058376110165ab5a5a0d56cd0b4a7c
Mariela <marielammcv.3@gmail.com>
2025-05-06 16:21:23 -0500

    welcome

--- mi-comedor/src/components/welcome/Welcome.tsx
+++ mi-comedor/src/components/welcome/Welcome.tsx
@@ -6,7 +6,7 @@ const Welcome: React.FC = () => {
         <div className="d-flex flex-column justify-content-center align-items-center vh-100 text-center">
             <h1 className="titulo-Pderecho  display-4">BIENVENIDOS</h1>
             <img
-                src="/assets/logo.png"
+                src="https://i.postimg.cc/4dsLbM1C/logo.jpg"
                 alt="Logo"
                 className="welcome-logo mt-3 img-fluid"
             />
