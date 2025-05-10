c1b6b96b383fcfaecd7a2eb45e4c2c5d3bdf50b2
EsmeraldaCienfuegos <esmeraldacielito2015@hotmail.com>
2025-05-10 18:01:22 -0500

    respondive Navbar

--- mi-comedor/src/App.tsx
+++ mi-comedor/src/App.tsx
@@ -38,12 +38,22 @@ const App: React.FC = () => {
         <AppBar position="static" sx={{ backgroundColor: "#F57C00" }}>
           <Toolbar>
             <Box display="flex" alignItems="center">
-              <img
-                src="https://i.postimg.cc/4dsLbM1C/logo.jpg"
-                alt="Logo"
-                className="navbar-logo"
-              />
+              {/* en tu JSX */}
+              <>
+                <img
+                  src="https://i.postimg.cc/4dsLbM1C/logo.jpg"
+                  className="navbar-logo"
+                  alt="Logo"
+                />
+                <img
+                  src="https://i.postimg.cc/cHR9Zcgf/png-Mesa-de-trabajo-1-4x.png"
+                  className="logo-mobile"
+                  alt="LogoMobile"
+                />
+              </>
+
               <Typography
+                className="navbar-title"
                 variant="h4"
                 component={Link}
                 to="/"
@@ -58,13 +68,17 @@ const App: React.FC = () => {
             </Box>
 
             <Box display="flex" alignItems="center" marginLeft="auto">
-              <Typography sx={{ fontSize: "22px", marginRight: "10px" }}>
+              <Typography
+                className="name-user"
+                sx={{ fontSize: "22px", marginRight: "10px" }}
+              >
                 {currentUser.name}
               </Typography>
 
               <Button
                 variant="contained"
                 startIcon={<LogoutIcon />}
+                className="cerrar-sesion-btn"
                 onClick={logOut}
                 sx={{
                   ml: 2,
