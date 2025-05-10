71c90ca9831f82c90f61ec02c5593613fc3aa637
EsmeraldaCienfuegos <esmeraldacielito2015@hotmail.com>
2025-05-10 18:51:11 -0500

    Avance profile

--- mi-comedor/src/components/Profile/Profile.tsx
+++ mi-comedor/src/components/Profile/Profile.tsx
@@ -1,28 +1,51 @@
 import React from "react";
-import { getCurrentUser } from "../../services/auth.service";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
+import { cards } from "../../pages/ProfilePage";
 
 const Profile: React.FC = () => {
-  const currentUser = getCurrentUser();
-  
-  if (!currentUser) {
-    return (
-      <div className="container">
-        <header className="jumbotron">
-          <h3>No user logged in.</h3>
-        </header>
-      </div>
-    );
-  }
-
+  const [selectedCard, setSelectedCard] = React.useState(0);
   return (
-    <div className="container">
-      <header className="jumbotron">
-        <h3>Welcome, <strong>{currentUser.username}</strong>!</h3>
-      </header>
-      <p>
-        <strong>Token:</strong> {currentUser.accessToken}
-      </p>
-    </div>
+    <Box
+      sx={{
+        width: "100%",
+        maxWidth: 800,
+        margin: "0 auto",
+        padding: 2,
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+        gap: 2,
+      }}
+    >
+      {cards.map((card, index) => (
+        <Card key={card.id}>
+          <CardActionArea
+            onClick={() => setSelectedCard(index)}
+            data-active={selectedCard === index ? "true" : undefined}
+            sx={{
+              height: "100%",
+              transition: "0.3s",
+              "&[data-active]": {
+                backgroundColor: "action.selected",
+                "&:hover": {
+                  backgroundColor: "action.selectedHover",
+                },
+              },
+            }}
+          >
+            <CardContent>
+              <Typography variant="h5">{card.title}</Typography>
+              <Typography variant="body2" color="text.secondary">
+                {card.description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      ))}
+    </Box>
   );
 };
 
---
+++ mi-comedor/src/pages/ProfilePage.tsx
@@ -0,0 +1,32 @@
+export const cards = [
+  {
+    id: 1,
+    title: "M1",
+    description: "Mis productos",
+  },
+  {
+    id: 2,
+    title: "M2",
+    description: "Raciones",
+  },
+  {
+    id: 3,
+    title: "M3",
+    description: "Beneficiarios",
+  },
+  {
+    id: 1,
+    title: "M4",
+    description: "Reporte del día y semanal",
+  },
+  {
+    id: 2,
+    title: "M5",
+    description: "Animals are a part of nature.",
+  },
+  {
+    id: 3,
+    title: "M6",
+    description: "Humans depend on plants and animals for survival.",
+  },
+];
