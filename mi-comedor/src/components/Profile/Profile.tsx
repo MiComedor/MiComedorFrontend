c5a3bf3f5deacdea4088f6f9e9b396524c5cf5ac
EsmeraldaCienfuegos <esmeraldacielito2015@hotmail.com>
2025-05-13 02:38:22 -0500

    diseño profile 100%

--- mi-comedor/src/components/Profile/Profile.tsx
+++ mi-comedor/src/components/Profile/Profile.tsx
@@ -6,31 +6,42 @@ import Typography from "@mui/material/Typography";
 import CardActionArea from "@mui/material/CardActionArea";
 import { cards } from "../../pages/ProfilePage";
 import "./Profile.css";
+import NotasProfile from "./NotasProfile";
+
 const Profile: React.FC = () => {
   const [selectedCard, setSelectedCard] = React.useState(0);
+
   return (
-    <Box className="profile-container">
-      {cards.map((card, index) => (
-        <Card key={card.id} className="card-button">
-          <CardActionArea
-            onClick={() => setSelectedCard(index)}
-            data-active={selectedCard === index ? "true" : undefined}
-            className="card-action"
-          >
-            <img
-              src={card.image}
-              alt={card.description}
-              className="card-image"
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                {card.description}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      ))}
-    </Box>
+    <>
+      {/* Tarjetas en grid */}
+      <Box className="profile-container">
+        {cards.map((card, index) => (
+          <Card key={card.id} className="card-button">
+            <CardActionArea
+              onClick={() => setSelectedCard(index)}
+              data-active={selectedCard === index ? "true" : undefined}
+              className="card-action"
+            >
+              <img
+                src={card.image}
+                alt={card.description}
+                className="card-image"
+              />
+              <CardContent>
+                <Typography className="card-title-modules">
+                  {card.description}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))}
+      </Box>
+
+      {/* Componente de notas (fuera del grid) */}
+      <Box mt={4}>
+        <NotasProfile />
+      </Box>
+    </>
   );
 };
 
